26ea666a3a212119e4a2858c49eecf3d56ca619b
changzi lu <changzi.lu@ge1.iut-cachan.u-psud.fr>
2018-11-06 11:39:33 +0100

    j'ai commencé la correction de ds

--- src/main.c
+++ src/main.c
@@ -2,8 +2,31 @@
 //
 
 #include "my_header.h"
-
+#include <stdio.h>
+long int calcul_du_min (long int entier1, long int entier2);
 int main(void)
 {
-    return 0;
+	long int min;
+	long int a;
+	long int b;
+	printf("premier nombre=");
+	scanf("%d", &a);
+	printf("deuxieme nombre=");
+	scanf("%d", &b);
+	min=calcul_du_min(a,b);
+	printf("min=%d",min);
+	return 0;
+}
+long int calcul_du_min(long int entier1, long int entier2)
+{
+	long int f;
+	if(entier2 > entier1)
+	{
+		f=entier1;
+	}
+	else
+	{
+		f=entier2;
+	}
+	return f;
 }

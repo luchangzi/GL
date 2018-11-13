64e2b071ee9ac06fb888f8f6287209fce6990a6f
luchangzi <changzi.lu@u-psud.fr>
2018-11-13 22:38:37 +0100

    finir exo_45

--- src/main.c
+++ src/main.c
@@ -3,13 +3,40 @@
 
 #include "my_header.h"
 #include "TP_lib.h"
+#include <stdio.h>
 
-void main(void)
+int main(void)
 {
-	short resultat , i=0;
-	while(i<0)
+	short int resultat=0 , i=0;
+	printf("its your turn!");
+	i= LancerDe();
+	if(i==1)
 	{
-		resultat=LancerDe();
-		i++;
+		printf("you lose! its not your turn for now!");
+	}
+	else
+	{
+	    while(i!=1)
+	{
+		if(i==2||i==4||i==6)
+		{
+			resultat= resultat+i;
+		}
+		else if(i==3)
+		{
+			resultat= resultat * 3;
+		}
+		else if(i==5&&resultat>=2)
+		{
+			resultat= resultat-2;
+		}
+		else if(i==5&&resultat<2)
+		{
+			i=LancerDe();
+		}
+		i=LancerDe();
+	}
+	printf("you lose! its not your turn for now!your score is %d",resultat);
+	return 0;
 	}
 }

6df2644e5d092b6d79432c8004c3934d5bff5812
changzi lu <changzi.lu@ge1.iut-cachan.u-psud.fr>
2018-11-27 11:45:50 +0100

    finir jusqua4

--- src/main.c
+++ src/main.c
@@ -2,8 +2,40 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
+#include "stdio.h"
 
 int main(void)
 {
-    return 0;
+    unsigned char i,i2,pareil=0,a,b;
+	short gagnante[6];
+	short jouer[6];
+	
+	initialiserTirage(); 
+	for(a=0;a<6;a++)
+	{
+		gagnante[a]=tirerNumero();
+	}
+		
+	initialiserTirage();
+
+	for(b=0;b<6;b++)
+	{
+		jouer[b]=tirerNumero();
+	}
+	
+
+	for(i=0;i<6;i++)
+	{
+		for(i2=0;i2<6;i2++)
+		{
+			if(jouer[i]==gagnante[i2])
+			{
+				pareil++;
+			}
+		}
+	}
+	printf("%d",pareil);
+	
+	return 0;
 }

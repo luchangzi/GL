9447133c8a9b657f8a01fcd59c016dfe2fd2a05a
changzi lu <changzi.lu@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:54:20 +0100

    finir exo79

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-    char annee2[100],nom[100],prenom[100],chaine2[100]="je m'appelle Lea Dupont et j'ai 19 ans",prenom1[100]="prenom",nom1[100]="nom",annee1[100]="annee de naissance";
+    char annee2[100],nom[100],prenom[100],prenom1[100]="prenom",nom1[100]="nom",annee1[100]="annee de naissance";
 	unsigned long int annee;
 	int a,b,c;
 	
@@ -15,7 +15,7 @@ int main(void)
 	scanf("%s%s%lu",nom,prenom,&annee);
 	sprintf(annee2,"%lu",annee);
 	printf("%s %s %lu\n",nom,prenom,annee);
-	printf("%s",chaine2);
+	printf("je m'appelle %s %s et j'ai19ans",nom,prenom);
 	a=MessagePopup(nom1,nom);
 	b=MessagePopup(prenom1,prenom);
 	c=MessagePopup(annee1,annee2);
